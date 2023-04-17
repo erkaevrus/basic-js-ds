ac29ac53e15ff260e94b60ea61497e200c4242ce
erkaevrus <erk_08@mail.ru>
2023-04-17 17:19:41 +0500

    add solutions

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,22 +12,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+
 class Queue {
+    constructor() {
+        this.head = null
+        this.tail = null
+    }
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    getUnderlyingList() {
+        return this.head
+    }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    enqueue(value) {
+        let node = new Node(value)
+        if(this.head) {
+            this.tail.next = node
+            this.tail = node
+        } else {
+            this.head = node
+            this.tail = node
+        }
+    }
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    dequeue() {
+        let deleteValue = this.head.value
+
+        if(this.head) {
+            this.head = this.head.next
+        }
+
+        return deleteValue
+    }
 }
 
 module.exports = {
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+
+function removeKFromList(list, k) {
+    let current = list
+    let prev = null
+
+    while(current) {
+        if (current.value === k) {
+            if(prev) {
+                prev.next = current.next
+            } else {
+                list = current.next
+            }
+        } else {
+            prev = current
+        }
+        current = current.next
+
+    }
+    return list
 }
 
 module.exports = {
